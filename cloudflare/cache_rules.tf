8967c9e8ad084ce397e73c92c4856a739ec57a71
Enoxime <4358598+Enoxime@users.noreply.github.com>
2026-01-11 11:19:42 -0500

    feat: Add immich-public-proxy plugin and adapt cloudflare to that

---
+++ cloudflare/cache_rules.tf
@@ -0,0 +1,17 @@
+resource "cloudflare_ruleset" "photos_immich" {
+  kind = "custom"
+  name = "No caching on /share/video for immich"
+  phase = "http_request_cache_settings"
+  account_id = var.account_id
+  description = "Bypass caching on /share/video for immich. See https://community.cloudflare.com/t/mp4-wont-load-in-safari-using-cloudflare/10587/48"
+  zone_id = var.zone_id
+  rules = [ {
+    action = "set_cache_settings"
+    expression = "(http.host eq \"photos.${var.domain}\" and starts_with(http.request.uri.path, \"/share/video\"))"
+    description = "Bypass caching on hostname photos.${var.domain} that starts with path /share/video"
+    enabled = true
+    action_parameters = {
+      cache = false
+    }
+  } ]
+}
